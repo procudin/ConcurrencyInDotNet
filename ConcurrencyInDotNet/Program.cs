4b19f235807302b3bc3dac893ba44e744e887efe
prokudin <prokudintema@yandex.ru>
2019-08-06 11:11:01 +0300

    Создан проект. Добавлена реализация агента для потокобезпасного добавления/удаления элементов из словаря.

---
+++ ConcurrencyInDotNet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ConcurrencyInDotNetFSharp;
+
+namespace ConcurrencyInDotNet
+{
+    class Program
+    {
+        static DictionaryAgent<int, string> agent = new DictionaryAgent<int, string>();
+
+        static async Task Main(string[] args)
+        {
+            await Task.Run(() =>
+            {
+                Enumerable.Repeat(0, 10000).ToList().AsParallel().WithDegreeOfParallelism(3).Select((v, i) => i % 10000).ForAll(v => agent.AddIfNotExists(v, v.ToString()));
+
+                agent.AddIfNotExists(10, 10.ToString());
+                agent.AddIfNotExists(20, 10.ToString());
+                Thread.Sleep(5000);
+            });
+        }
+    }
+
+
+    public static class EXT
+        {
+        public static void ForEach<T>(this IEnumerable<T> col, Action<T> act)
+        {
+            foreach (var t in col)
+                act(t);
+        }
+            
+
+        }
+}
---
+++ ConcurrencyInDotNetFSharp/DictionaryAgent.fs
@@ -0,0 +1,33 @@
+﻿namespace ConcurrencyInDotNetFSharp
+
+open System.Collections.Generic
+
+module private DictionaryAgentMessageModule =
+    type DictionaryAgentMessage<'k, 'v> =
+        | AddIfNotExists of key:'k * value:'v
+        | RemoveIfExists of key:'k
+
+// Агент для потокобезопасной записи/удаления записей из словаря (решение проблемы производителя-потребителя без использования иммутабельных/конкуррентных коллекций)
+// Используется ассинхронная очередь для аккумулирования сообщений, поступающих из конкуррнтной среды, для их дальнейшей обработки
+type DictionaryAgent<'k, 'v> when 'k: equality () =
+    let agent = MailboxProcessor<DictionaryAgentMessageModule.DictionaryAgentMessage<'k, 'v>>.Start(fun inbox ->
+        let dict = Dictionary<'k, 'v>()
+        let rec loop() = async {
+            let! msg = inbox.Receive()
+            match msg with
+            | DictionaryAgentMessageModule.AddIfNotExists(key, value) ->
+                let exists, _ = dict.TryGetValue(key)
+                if not exists then
+                    dict.Add(key, value)                       
+            | DictionaryAgentMessageModule.RemoveIfExists(key) ->
+                let exists, _ = dict.TryGetValue(key)
+                if (exists) then
+                    dict.Remove(key) |> ignore        
+            return! loop()        
+        }
+        loop()
+    )     
+    member this.AddIfNotExists(key, value) = agent.Post(DictionaryAgentMessageModule.AddIfNotExists(key, value))
+    member this.RemoveIfExists(key) = agent.Post(DictionaryAgentMessageModule.RemoveIfExists(key))     
+    
+
